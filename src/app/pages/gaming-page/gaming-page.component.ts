19c4ebce4ac17c202966910898bce4d2f8076a1a
Tom Williams <thomas.williams@proav.com>
2025-02-07 20:08:31 +0000

    feat(Home) Added some pages and some basics

---
+++ src/app/pages/gaming-page/gaming-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atom-gaming-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './gaming-page.component.html',
+  styleUrl: './gaming-page.component.scss'
+})
+export class GamingPageComponent {
+
+}
