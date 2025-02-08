cc6aa5b838e5a5bc96dd997b435d93e0f397082e
Tom Williams <thomas.williams@proav.com>
2025-02-08 23:42:14 +0000

    feat: Complete

--- src/app/pages/bunito-page/bunito-page.component.ts
+++ src/app/pages/bunito-page/bunito-page.component.ts
@@ -3,6 +3,7 @@ import {ThemeModeToggleComponent} from "../../single-elements/theme-mode-toggle/
 import {SpeedDial} from "primeng/speeddial";
 import {LoveButtonComponent} from "../../single-elements/love-button/love-button.component";
 import {ValentineFormComponent} from "../../bunito-page-components/valentine-form/valentine-form.component";
+import {GalleriaModule, GalleriaResponsiveOptions} from "primeng/galleria";
 
 @Component({
   selector: 'atom-bunito-page',
@@ -10,12 +11,41 @@ import {ValentineFormComponent} from "../../bunito-page-components/valentine-for
     ThemeModeToggleComponent,
     SpeedDial,
     LoveButtonComponent,
-    ValentineFormComponent
+    ValentineFormComponent,
+    GalleriaModule
   ],
   templateUrl: './bunito-page.component.html',
   styleUrl: './bunito-page.component.scss'
 })
 export class BunitoPageComponent {
+  showLove: boolean = false;
+  responsiveOptions: GalleriaResponsiveOptions[] | undefined;
+  images: any[] = [
+    {
+      src: 'assets/images/bunito/bunito-1.jpg',
+      title: 'Bunito 1'
+    },
+    {
+      src: 'assets/images/bunito/bunito-2.jpg',
+      title: 'Bunito 2'
+    },
+    {
+      src: 'assets/images/bunito/bunito-3.jpg',
+      title: 'Bunito 3'
+    },
+    {
+      src: 'assets/images/bunito/bunito-4.jpg',
+      title: 'Bunito 4'
+    },
+    {
+      src: 'assets/images/bunito/bunito-5.jpg',
+      title: 'Bunito 5'
+    },
+    {
+      src: 'assets/images/bunito/bunito-6.jpg',
+      title: 'Bunito 6'
+    },
+  ]
   showScreenMask: boolean = false;
   speedDialItems: any[] = [
     {
@@ -28,10 +58,21 @@ export class BunitoPageComponent {
 
   onHideSpeedDial() {
     console.log('hide');
-    this.showScreenMask = false;
   }
 
   onShowSpeedDial() {
     this.showScreenMask = true;
+  }
+
+  onLoveButtonClick() {
+    this.showLove = true;
+  }
+
+  onHideMask($event: MouseEvent) {
+    const galleryContainer = document.querySelector('.gallery-container');
+    if (!galleryContainer?.contains($event.target as Node)) {
+      this.showScreenMask = false;
+      this.showLove = false;
+    }
   }
 }
